fa7e7b8f6e2ba2f0231e8e92ab89add8f1e4e8e6
Linus Elvius <135825781+linx02@users.noreply.github.com>
2025-05-19 14:53:36 +0200

    Initial commit

---
+++ src/main/java/com/caw24g/grupp_uppgift/GruppUppgiftApplication.java
@@ -0,0 +1,13 @@
+package com.caw24g.grupp_uppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppUppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruppUppgiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caw24g/grupp_uppgift/GruppUppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caw24g.grupp_uppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppUppgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
